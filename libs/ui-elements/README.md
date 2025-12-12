85d8248ab83d57c2e7b6824f0609a4964285089d
w.alexander <will_alexander@me.com>
2025-12-12 21:25:48 +0100

    Add ui-elements lib

---
+++ libs/ui-elements/README.md
@@ -0,0 +1,3 @@
+# ui-elements
+
+This library was generated with [Nx](https://nx.dev).
---
+++ libs/ui-elements/src/lib/ui-elements/ui-elements.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui-elements',
+  imports: [],
+  templateUrl: './ui-elements.html',
+  styleUrl: './ui-elements.css',
+})
+export class UiElements {}
