6fada6c3518d44c67f089b6cacda805a87961a9e
w.alexander <will_alexander@me.com>
2025-12-19 21:51:07 +0100

    Add colon to page header

--- libs/ui-elements/src/header/header.ts
+++ libs/ui-elements/src/header/header.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ninja-header',
   template: `
     <header class="h-12 px-4 py-2">
-      <h1 class="text-2xl font-bold text-primary-700">PROJECT NINJA</h1>
+      <h1 class="text-2xl font-bold text-primary-700">PROJECT: NINJA</h1>
     </header>
   `
 })
