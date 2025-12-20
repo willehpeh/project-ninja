561a1c3b35419cdb68b8cd80c7526a1c93527be5
w.alexander <will_alexander@me.com>
2025-12-20 23:04:50 +0100

    Remove unnecessary files from tests package

--- tests/src/lib/tests.spec.ts
+++
@@ -1,7 +0,0 @@
-import { tests } from './tests';
-
-describe('tests', () => {
-  it('should work', () => {
-    expect(tests()).toEqual('tests');
-  });
-});
--- tests/src/lib/tests.ts
+++
@@ -1,3 +0,0 @@
-export function tests(): string {
-  return 'tests';
-}
