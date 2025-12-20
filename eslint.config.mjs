cf2d203422afe6337d0a5937bbc7a4889c1b80ab
w.alexander <will_alexander@me.com>
2025-12-20 23:25:03 +0100

    Update dependency constraints

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,64 @@
-import nx from "@nx/eslint-plugin";
+import nx from '@nx/eslint-plugin';
 
 export default [
-    ...nx.configs["flat/base"],
-    ...nx.configs["flat/typescript"],
-    ...nx.configs["flat/javascript"],
-    {
-        ignores: [
-            "**/dist",
-            "**/out-tsc"
-        ]
-    },
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.js",
-            "**/*.jsx"
-        ],
-        rules: {
-            "@nx/enforce-module-boundaries": [
-                "error",
-                {
-                    enforceBuildableLibDependency: true,
-                    allow: [
-                        "^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$"
-                    ],
-                    depConstraints: [
-                        {
-                            sourceTag: "*",
-                            onlyDependOnLibsWithTags: [
-                                "*"
-                            ]
-                        }
-                    ]
-                }
-            ]
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: [
+      '**/dist',
+      '**/out-tsc'
+    ]
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.js',
+      '**/*.jsx'
+    ],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [
+            '^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'
+          ],
+          depConstraints: [
+            {
+              sourceTag: 'layer:domain',
+              onlyDependOnLibsWithTags: []
+            },
+            {
+              sourceTag: 'layer:application',
+              onlyDependOnLibsWithTags: ['layer:domain']
+            },
+            {
+              sourceTag: 'layer:infrastructure',
+              onlyDependOnLibsWithTags: ['layer:application', 'layer:domain']
+            },
+            {
+              sourceTag: 'layer:tests',
+              onlyDependOnLibsWithTags: ['layer:domain', 'layer:application', 'layer:infrastructure']
+            }
+          ]
         }
-    },
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.cts",
-            "**/*.mts",
-            "**/*.js",
-            "**/*.jsx",
-            "**/*.cjs",
-            "**/*.mjs"
-        ],
-        // Override or add rules here
-        rules: {}
+      ]
     }
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs'
+    ],
+    // Override or add rules here
+    rules: {}
+  }
 ];
