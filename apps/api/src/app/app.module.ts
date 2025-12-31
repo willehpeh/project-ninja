96c2c7cc6b353b5599a44c45b0ee52fb4f663e16
w.alexander <will_alexander@me.com>
2025-12-31 21:53:51 +0100

    Add empty Team module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EventStoreModule } from '@ninja-4-vs/infrastructure';
 import { CqrsModule } from '@nestjs/cqrs';
+import { TeamModule } from './delivery/teams/team.module';
 
 @Module({
   imports: [
     CqrsModule.forRoot(),
     EventStoreModule.forRoot({
       basePath: './data'
-    })
+    }),
+    TeamModule
   ]
 })
 export class AppModule {
